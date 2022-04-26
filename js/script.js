0097758a8fc2cb52f9361ef53349996f9d1dc84e
DoshTT <jos.alvarezd@duocuc.cl>
2022-04-26 03:56:17 -0400

    VALIDACIONES TERMINADAS SIN RESPONSIVIDAD COMPLETA

--- js/script.js
+++ js/script.js
@@ -1,40 +1,102 @@
-/* VALIDACION CON JQUERY REGISTRO E INICIO DE SESION*/
+/* VALIDACION CON JQUERY REGISTRO */
+//Validacion campos de registro
 jQuery.validator.addMethod("customEmail", function(value, element) {
     return this.optional(element) || /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/.test(value);
-}, "Please enter valid email address!");
+}, "Ingrese un correo válido ");
 $(document).ready(function() {
     $("#registration").validate({
-        rules: {
-            nombres: {
-                required: true
+            rules: {
+                nombres: {
+                    required: true
+                },
+
+                apellidos: {
+                    required: true
+                },
+
+                correo: {
+                    required: true,
+                    customEmail: true
+                },
+
+                password: {
+                    required: true
+                },
+                confirm: {
+                    required: true,
+                    equalTo: '#password'
+                }
             },
 
-            apellidos: {
-                required: true
-            },
+            messages: {
+                nombres: {
+                    required: 'Por favor ingrese su nombre!'
+                },
+                apellidos: {
+                    required: 'Por favor ingrese su apellido'
+                },
+                correo: {
+                    required: 'Por favor ingrese su correo',
 
-            correo: {
-                required: true,
-                customEmail: true
-            },
+                },
+                password: {
+                    required: 'Por favor ingrese una contraseña'
+                },
+                confirm: {
+                    required: 'Por favor reingrese la contraseña',
+                    equalTo: 'Ingrese la misma contraseña que escribió arriba '
 
-            password: {
-                required: true
-            },
-            confirm: {
-                required: true,
-                equalTo: '#password'
+                }
             }
-        },
 
-        messages: {
-            nombres: "Por favor ingrese su nombre"
+        })
+        //Validacion Checkbox
+    $(function() {
+        $("#btn").click(function() {
+            //Reference the Group of CheckBoxes and verify whether at-least one CheckBox is checked.
+            var checked = $("#registration input[type=checkbox]:checked").length;
 
-        },
-    })
+            //Set the Valid Flag to True if at-least one CheckBox is checked.
+            var isValid = checked > 0;
+
+            //Display error message if no CheckBox is checked.
+            $("#spnError")[0].style.display = isValid ? "none" : "block";
+        });
+    });
 
 
     $('#btn').click(function() {
         $("#registration").validate();
+    });
+
+
+});
+/* VALIDACION CON JQUERY INICIO SESION */
+$(document).ready(function() {
+    $("#inicio-sesion").validate({
+        rules: {
+            correo1: {
+                required: true,
+                customEmail: true
+            },
+
+            password1: {
+                required: true
+            },
+        },
+
+        messages: {
+            correo1: {
+                required: 'Por favor ingrese su correo',
+
+            },
+            password1: {
+                required: 'Por favor ingrese una contraseña'
+            },
+        }
+
+    })
+    $('#btn2').click(function() {
+        $("#inicio-sesion").validate();
     });
 });
